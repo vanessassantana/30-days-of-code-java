fbb9755f8b1ff374b6164f21fc58c2072bbf1b27
vanessa <vanessasantana12@outlook.com>
2025-05-11 15:10:45 -0300

    feat: adicionar solução do Dia 0 - Hello, World

---
+++ resolucao_desafios/src/resolucao_desafios/Day00HelloWorld.java
@@ -0,0 +1,18 @@
+package resolucao_desafios;
+
+import java.util.Scanner;
+
+public class Day00HelloWorld {
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			
+			String inputString = scanner.nextLine();
+			
+			System.out.println("Hello, World.");
+			System.out.println(inputString);
+			scanner.close();
+
+
+     }
+}
