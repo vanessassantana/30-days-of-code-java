7bd50fc7597518a28275d2dbce9c3753b1a23289
vanessa <vanessasantana12@outlook.com>
2025-05-13 20:30:53 -0300

    Adicionar solução do Dia 01 - DataTypes

--- resolucao_desafios/src/resolucao_desafios/Day01DataTypes.java
+++ resolucao_desafios/src/resolucao_desafios/Day01DataTypes.java
@@ -1,5 +1,36 @@
 package resolucao_desafios;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Day01DataTypes {
+
+	public static void main(String[] args) {
+		 Locale.setDefault(Locale.US); 
+	     
+	     
+	     int i = 4;
+	     double d = 4.0;
+	     String s = "HackerRank ";
+	     
+	     Scanner input = new Scanner(System.in);
+
+	     int i2;
+	     double d2;
+	     String s2;
+	     
+	     i2 = input.nextInt();       
+	     d2 = input.nextDouble();    
+	     input.nextLine();           
+	     s2 = input.nextLine();  
+	     
+	     System.out.println(i + i2);                   
+	     System.out.printf("%.1f\n", d + d2);         
+	     System.out.println(s + s2);                  
+
+	        input.close();
+
+
+	}
 
 }
